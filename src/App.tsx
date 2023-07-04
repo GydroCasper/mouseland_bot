8413d2ba68222e3cdf9a250ee81f311f1f2d6d7b
GydroCasper <gydrocasper@outlook.com>
2023-07-04 18:30:06 -0400

    add shift component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { useEffect } from "react"
+// import "./App.css"
+
+import { Shift } from "./components/Shift"
+
+// declare global {
+//   interface Window {
+//       Telegram: any;
+//   }
+// }
+
+// const telegram = window.Telegram.WebApp
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // useEffect(() => telegram.ready(), [])
+
+  return <Shift />
 }
 
-export default App;
+export default App
---
+++ src/components/Shift.tsx
@@ -0,0 +1,5 @@
+export const Shift = () => {
+  console.log(process.env.REACT_APP_SHIFT_PERIOD)
+
+  return null
+}
