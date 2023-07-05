d55797de69d6e80d5004f87bf763bf818dcdbd15
GydroCasper <gydrocasper@outlook.com>
2023-07-05 19:43:28 -0400

    firebase added

--- src/components/FoodEvent.tsx
+++ src/components/FoodEvent.tsx
@@ -4,11 +4,33 @@ interface FoodEventProps {
   name: string
 }
 
+declare global {
+  interface Window {
+    Telegram: {
+      WebAppUser: any
+    }
+  }
+}
+
 export const FoodEvent: React.FC<FoodEventProps> = ({ name }) => {
+  const s = window.Telegram.WebAppUser
+  console.log(s)
+  const handleAssingMeButtonClick = () => {
+    console.log("adsfadsf")
+  }
+
   return (
     <div>
       <div>{name}</div>
-      <div><Button variant="contained" size="small">Assign me</Button></div>
+      <div>
+        <Button
+          variant="contained"
+          size="small"
+          onClick={handleAssingMeButtonClick}
+        >
+          Assign me
+        </Button>
+      </div>
     </div>
   )
 }
